ad0c7e28f66efd86a0ba727f2e11fd9ebb9630c5
Mengdan <zhaomengdan93@gmail.com>
2014-08-27 17:20:49 +0800

    factory user entity edited

--- IslandFurnitureERPSytem/IslandFurnitureERPSytem-ejb/src/java/entity/FactoryUserEntity.java
+++ IslandFurnitureERPSytem/IslandFurnitureERPSytem-ejb/src/java/entity/FactoryUserEntity.java
@@ -8,52 +8,21 @@ package entity;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
 /**
  *
  * @author dan
  */
 @Entity
-public class FactoryUserEntity implements Serializable {
+@Table(name="F_user")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class FactoryUserEntity extends UserEntity implements Serializable {
     private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof FactoryUserEntity)) {
-            return false;
-        }
-        FactoryUserEntity other = (FactoryUserEntity) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entity.FactoryUserEntity[ id=" + id + " ]";
+    public FactoryUserEntity() {
     }
     
 }
