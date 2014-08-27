71f2263c4b88d6e55a96eac26215e5b3350d0de9
summerz8 <ms.z.summer@gmail.com>
2014-08-27 22:52:12 +0800

    Create SuperUserClient as a system internal administrator

---
+++ SuperUserClient/src/java/superuserclient/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package superuserclient;
+
+import SessionBean.IFManagerBeanRemote;
+
+/**
+ *
+ * @author zhangshiyu
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
