d69f65e68ea65c82bfb4a891e26787c76a71542d
Hang Sun <sunhang36@gmail.com>
2014-09-05 12:30:01 +0800

    Create Factory Factory Bin

---
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Factory/FactoryBinFinishedGoodEntity.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entity.Factory;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author hangsun
+ */
+@Entity
+public class FactoryBinFinishedGoodEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long factoryID;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String factoryBinID;
+    
+    private List<Entity> items;
+    
+    private FactoryEntity factory = new FactoryEntity();
+
+    public Long getFactoryID() {
+        return factoryID;
+    }
+
+    public void setFactoryID(Long factoryID) {
+        this.factoryID = factoryID;
+    }
+    
+    public String getFactoryBinID() {
+        return factoryBinID;
+    }
+
+    public void setFactoryBinID(String factoryBinID) {
+        this.factoryBinID = factoryBinID;
+    }
+    
+    public List<Entity> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Entity> items) {
+        this.items = items;
+    }
+    
+    @ManyToOne
+    public FactoryEntity getFactory(){
+         return factory;
+    }
+    
+    public void setFactory(FactoryEntity factory){
+         this.factory = factory;
+    }
+    
+    @Override
+    public String toString() {
+        return "Entity.Factory.FactoryBinEntity[ factoryBinId=" + factoryBinID + " ]" + "[ factoryId=" + factoryID + " ]";
+    }
+    
+}
---
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Factory/FactoryEntity.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entity.Factory;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author zhangshiyu
+ */
+@Entity
+@Table(name = "factory")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class FactoryEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long factoryID;
+    private String country;
+    private String address;
+    private String contact;
+    private String manager;
+    private Collection<FactoryBinFinishedGoodEntity> factoryBinFinishedGood = new ArrayList<FactoryBinFinishedGoodEntity>();
+    private Collection<FactoryBinRawMaterialEntity> factoryBinRawMaterial = new ArrayList<FactoryBinRawMaterialEntity>();
+    private Collection<FactoryBinRetailProductEntity> factoryBinRetailProduct = new ArrayList<FactoryBinRetailProductEntity>();
+    
+    public FactoryEntity() {
+    }
+    
+    public long getFactoryId() {
+        return factoryID;
+    }
+
+    public void setUserId(long factoryID) {
+        this.factoryID = factoryID;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+     
+    @OneToMany(cascade={CascadeType.ALL}, mappedBy="factory")
+    public Collection<FactoryBinFinishedGoodEntity> getFactoryBinFinishedGood(){
+        return factoryBinFinishedGood;
+    }
+    
+    public void setFactoryBinFinishedGood(Collection<FactoryBinFinishedGoodEntity> factoryBinFinishedGood){
+        this.factoryBinFinishedGood = factoryBinFinishedGood; 
+    }
+    
+    @OneToMany(cascade={CascadeType.ALL}, mappedBy="factory")
+    public Collection<FactoryBinRawMaterialEntity> getFactoryBinRawMaterial(){
+        return factoryBinRawMaterial;
+    }
+    
+    public void setFactoryBinRawMaterial(Collection<FactoryBinRawMaterialEntity> factoryBinRawMaterial){
+        this.factoryBinRawMaterial = factoryBinRawMaterial; 
+    }
+    @OneToMany(cascade={CascadeType.ALL}, mappedBy="factory")
+    public Collection<FactoryBinRetailProductEntity> getFactoryBinRetailProduct(){
+        return factoryBinRetailProduct;
+    }
+    
+    public void setFactoryBinRetailProduct(Collection<FactoryBinRetailProductEntity> factoryBinRetailProduct){
+        this.factoryBinRetailProduct = factoryBinRetailProduct; 
+    }
+    
+    @Override
+    public String toString() {
+        return "entity.UserEntity[ id=" + factoryID + " ]";
+    }
+    
+}
