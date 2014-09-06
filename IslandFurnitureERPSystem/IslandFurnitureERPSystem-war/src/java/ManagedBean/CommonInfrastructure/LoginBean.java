7c058a0b36eb50c5e0fe33d1be765453395e4ecd
Mengdan <zhaomengdan93@gmail.com>
2014-09-06 20:48:55 +0800

    Store entities added

--- IslandFurnitureERPSystem/IslandFurnitureERPSystem-war/src/java/ManagedBean/CommonInfrastructure/LoginBean.java
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-war/src/java/ManagedBean/CommonInfrastructure/LoginBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ManagedBean;
+package ManagedBean.CommonInfrastructure;
 
 import SessionBean.IFManagerBeanRemote;
 import com.sun.xml.ws.runtime.dev.Session;
