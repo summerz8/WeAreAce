fd7fffb91d39a69bb1c9e5db819761ab87dbbbd5
Joe <hejinqiaoinsg@gmail.com>
2014-09-11 19:57:01 +0800

    no message

---
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Factory/MRP/WeeklyProductionPlanEntity.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entity.Factory.MRP;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author apple
+ */
+@Entity
+public class WeeklyProductionPlanEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String month;
+    private Integer week;
+    private Integer workingDayInWeek;
+    private Integer workingDayInMonth;
+    private Integer weeklyDemand;
+    private ProductionPlanEntity productionPlan;
+    private List<WeeklyRawMaterialRecordEntity> weeklyRawMaterialRecord;
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public Integer getWeek() {
+        return week;
+    }
+
+    public void setWeek(Integer week) {
+        this.week = week;
+    }
+
+    public Integer getWorkingDayInWeek() {
+        return workingDayInWeek;
+    }
+
+    public void setWorkingDayInWeek(Integer workingDayInWeek) {
+        this.workingDayInWeek = workingDayInWeek;
+    }
+
+    public Integer getWorkingDayInMonth() {
+        return workingDayInMonth;
+    }
+
+    public void setWorkingDayInMonth(Integer workingDayInMonth) {
+        this.workingDayInMonth = workingDayInMonth;
+    }
+
+    public Integer getWeeklyDemand() {
+        return weeklyDemand;
+    }
+
+    public void setWeeklyDemand(Integer weeklyDemand) {
+        this.weeklyDemand = weeklyDemand;
+    }
+
+    public ProductionPlanEntity getProductionPlan() {
+        return productionPlan;
+    }
+
+    public void setProductionPlan(ProductionPlanEntity productionPlan) {
+        this.productionPlan = productionPlan;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    
+    
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof WeeklyProductionPlanEntity)) {
+            return false;
+        }
+        WeeklyProductionPlanEntity other = (WeeklyProductionPlanEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Entity.Factory.MRP.WeeklyProductionPlanEntity[ id=" + id + " ]";
+    }
+    
+}
---
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Factory/MRP/WeeklyRawMaterialRecordEntity.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entity.Factory.MRP;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author apple
+ */
+@Entity
+public class WeeklyRawMaterialRecordEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Integer weeklyDemand;
+    private Double grossRequirement;
+    private Double plannedReceipts;
+    private Double scheduledReceipts;
+    private Double onHand;
+    private Double plannedOrder;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getWeeklyDemand() {
+        return weeklyDemand;
+    }
+
+    public void setWeeklyDemand(Integer weeklyDemand) {
+        this.weeklyDemand = weeklyDemand;
+    }
+
+    public Double getGrossRequirement() {
+        return grossRequirement;
+    }
+
+    public void setGrossRequirement(Double grossRequirement) {
+        this.grossRequirement = grossRequirement;
+    }
+
+    public Double getPlannedReceipts() {
+        return plannedReceipts;
+    }
+
+    public void setPlannedReceipts(Double plannedReceipts) {
+        this.plannedReceipts = plannedReceipts;
+    }
+
+    public Double getScheduledReceipts() {
+        return scheduledReceipts;
+    }
+
+    public void setScheduledReceipts(Double scheduledReceipts) {
+        this.scheduledReceipts = scheduledReceipts;
+    }
+
+    public Double getOnHand() {
+        return onHand;
+    }
+
+    public void setOnHand(Double onHand) {
+        this.onHand = onHand;
+    }
+
+    public Double getPlannedOrder() {
+        return plannedOrder;
+    }
+
+    public void setPlannedOrder(Double plannedOrder) {
+        this.plannedOrder = plannedOrder;
+    }
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof WeeklyRawMaterialRecordEntity)) {
+            return false;
+        }
+        WeeklyRawMaterialRecordEntity other = (WeeklyRawMaterialRecordEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity.Factory.MRP.WeeklyRawMaterialRecord[ id=" + id + " ]";
+    }
+    
+}
