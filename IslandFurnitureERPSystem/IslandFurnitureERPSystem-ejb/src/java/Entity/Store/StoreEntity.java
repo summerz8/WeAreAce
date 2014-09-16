9ef762d1b93ebc89535a168467f919419ee0aa0d
Mengdan <zhaomengdan93@gmail.com>
2014-09-17 06:28:10 +0800

    FactoryEntity , StoreEntity, StoreProductEntity relation changed

--- IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Store/StoreEntity.java
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Store/StoreEntity.java
@@ -6,15 +6,16 @@
 package Entity.Store;
 
 import Entity.Factory.FactoryEntity;
-import java.util.List;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -35,9 +36,13 @@ public class StoreEntity implements Serializable {
     private String manager;
     private Boolean deleteFlag;
 
-    //factory entity -- store entity: M <--> M 
-    @ManyToMany(cascade = {CascadeType.PERSIST}, mappedBy = "stores")
-    private List<FactoryEntity> factorys = new ArrayList<>();
+//    //factory entity -- store entity: M <--> M 
+//    @ManyToMany(cascade = {CascadeType.PERSIST}, mappedBy = "stores")
+//    private List<FactoryEntity> factorys = new ArrayList<>();
+    
+    //store entity -- store product entity: 1 <--> M
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy = "stores")
+    private List<StoreProductEntity> storeProduct = new ArrayList<>();
     
     public StoreEntity() {
     }
@@ -66,13 +71,15 @@ public class StoreEntity implements Serializable {
         this.address = address;
     }
 
-    public List<FactoryEntity> getFactorys() {
-        return factorys;
+    public List<StoreProductEntity> getStoreProduct() {
+        return storeProduct;
     }
 
-    public void setFactorys(List<FactoryEntity> factorys){
-        this.factorys = factorys;
+    public void setStoreProduct(List<StoreProductEntity> storeProduct) {
+        this.storeProduct = storeProduct;
     }
+
+   
 
     public String getCountry() {
         return country;
--- IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Store/StoreProductEntity.java
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Store/StoreProductEntity.java
@@ -6,6 +6,7 @@
 
 package Entity.Store;
 
+import Entity.Factory.FactoryEntity;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -22,35 +24,60 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "StoreProduct")
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class StoreProductEntity extends StoreInventoryEntity implements Serializable {
+public class StoreProductEntity  implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storeProductId;
+    
+    //store product entity -- factory entity: M <--> 1 
+    @ManyToOne
+    private FactoryEntity factory;
+    
+    //store product entity -- stores entity: M <--> 1
+    @ManyToOne
+    private StoreEntity stores;
 
-    public Long getId() {
-        return id;
+    public Long getStoreProductId() {
+        return storeProductId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setStoreProductId(Long storeProductId) {
+        this.storeProductId = storeProductId;
     }
 
+    public FactoryEntity getFactory() {
+        return factory;
+    }
+
+    public void setFactory(FactoryEntity factory) {
+        this.factory = factory;
+    }
+
+    public StoreEntity getStores() {
+        return stores;
+    }
+
+    public void setStores(StoreEntity stores) {
+        this.stores = stores;
+    }
+
+    
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (storeProductId != null ? storeProductId.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        // TODO: Warning - this method won't work in the case the storeProductId fields are not set
         if (!(object instanceof StoreProductEntity)) {
             return false;
         }
         StoreProductEntity other = (StoreProductEntity) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.storeProductId == null && other.storeProductId != null) || (this.storeProductId != null && !this.storeProductId.equals(other.storeProductId))) {
             return false;
         }
         return true;
@@ -58,7 +85,7 @@ public class StoreProductEntity extends StoreInventoryEntity implements Serializ
 
     @Override
     public String toString() {
-        return "Entity.Store.StoreProductEntity[ id=" + id + " ]";
+        return "Entity.Store.StoreProductEntity[ id=" + storeProductId + " ]";
     }
     
 }
