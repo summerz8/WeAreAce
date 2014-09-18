f0d97667591e0270405bd8fcd53bf766e22b0be4
summerz8 <ms.z.summer@gmail.com>
2014-09-18 12:17:12 +0800

    modify PurchasedItemAndSuppleirManagementModule.java and its local interface

--- IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/SessionBean/SCM/PurchasedItemAndSupplierManagementModuleLocal.java
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/SessionBean/SCM/PurchasedItemAndSupplierManagementModuleLocal.java
@@ -14,6 +14,7 @@
 package SessionBean.SCM;
 
 import java.util.Calendar;
+import java.util.Collection;
 import javax.ejb.Local;
 
 /**
@@ -23,14 +24,20 @@ import javax.ejb.Local;
 @Local
 public interface PurchasedItemAndSupplierManagementModuleLocal {
 
+    public Collection<Object> viewItemwithSelectType(Long factoryId, String itemType) throws Exception;
+
     public String addSupplier(String itemType, Long itemId, String name, String address, String telephone, String fax, String remark, Double contractPrice, Integer leadTime, String unit, Calendar contractStartDate, Calendar contractEndDate) throws Exception;
 
-    public String editSupplier(Long supplierId, String name, String address, String telephone, String fax) throws Exception;
+    public String editSupplier(Long supplierId, String name, String address, String telephone, String fax, String remark) throws Exception;
 
     public String deleteSupplier(Long supplierId) throws Exception;
-    
-    public String addItem(Long factoryId, Long supplierId, String itemType, Long itemId, Double contractPrice,  Integer leadTime, String unit, Calendar contractStartDate, Calendar contractEndDate) throws Exception;
-    
+
+    public Collection<Object> viewItemListNotInFactory(Long factoryId, String itemType) throws Exception;
+
+    public String addItem(Long factoryId, String itemType, Long itemId) throws Exception;
+
+    public String addContract(Long factoryId, Long supplierId, String itemType, Long itemId, Double contractPrice, Integer leadTime, Calendar contractStartDate, Calendar contractEndDate) throws Exception;
+
     public String deleteItem(String itemType, Long itemFactoryId) throws Exception;
-            
+
 }
