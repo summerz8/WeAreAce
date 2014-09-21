ba1a5d7989256d6f8e094a2507e7e1054f3d3150
Mengdan <zhaomengdan93@gmail.com>
2014-09-21 20:43:57 +0800

    AddUserAccountBean moved to ManagedBean.CommonInfrastructure.EnterpriseResourceControl package

--- IslandFurnitureERPSystem/IslandFurnitureERPSystem-war/src/java/ManagedBean/CommonInfrastructure/EnterpriseResourceControl/AddUserAccountBean.java
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-war/src/java/ManagedBean/CommonInfrastructure/EnterpriseResourceControl/AddUserAccountBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ManagedBean.CommonInfrastructure;
+package ManagedBean.CommonInfrastructure.EnterpriseResourceControl;
 
 import Entity.CommonInfrastructure.UserEntity;
 import SessionBean.CommonInFrastructure.InternalUserAccountManagementModuleLocal;
