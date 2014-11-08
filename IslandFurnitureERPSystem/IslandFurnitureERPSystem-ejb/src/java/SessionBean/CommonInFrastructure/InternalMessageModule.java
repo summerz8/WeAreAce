5800f9e8cc27897344b92dda0eedf0c7b90b6705
Yoky Zhang <zhangyaowen0707@gmail.com>
2014-11-08 20:25:42 +0800

    KM

--- IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/SessionBean/CommonInFrastructure/InternalMessageModule.java
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/SessionBean/CommonInFrastructure/InternalMessageModule.java
@@ -40,7 +40,7 @@ import javax.persistence.Query;
 
  */
 @Stateless
-public class InternalMessageModule implements InternalMessageModuleLocal {
+public class InternalMessageModule implements InternalMessageModuleLocal,InternalMessageModuleRemote {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
