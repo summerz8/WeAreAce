00496f1f378aeadd95a368ed580eb58543dfdf09
Zhang Shiyu <ms.z.summer@gmail.com>
2014-11-12 07:51:03 +0800

    surprise

--- IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Store/EventEntity.java
+++ IslandFurnitureERPSystem/IslandFurnitureERPSystem-ejb/src/java/Entity/Store/EventEntity.java
@@ -8,7 +8,6 @@ package Entity.Store;
 import java.io.Serializable;
 import java.util.Calendar;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
